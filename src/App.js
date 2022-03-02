36c29c90d60159240b338743455f84bd617d6c55
Ifeanyi Umeh <somto_umeh@yahoo.com>
2022-03-02 01:17:22 -0500

    Finished the adding calculator exercixe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(number1 + number2);
+
+  function handleNumber1change(e) {
+    setNumber1(Number(e.target.value));
+  }
+
+  function handleNumber2change(e) {
+    setNumber2(+e.target.value);
+  }
+  function calculateTotal() {
+    setTotal(number1 + number2);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Adding two numbers</h1>
+
+      <div className="number-inputs">
+        <input type="number" placeholder="0" onChange={handleNumber1change} />
+        <input type="number" placeholder="0" onChange={handleNumber2change} />
+      </div>
+
+      <button onClick={calculateTotal}>Add EM </button>
+
+      <h2>{total}</h2>
     </div>
   );
 }
